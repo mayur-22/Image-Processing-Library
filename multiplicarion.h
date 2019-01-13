72b6e6275d6bfee37a6cca2cc9a875d8f3725274
Mayur Solanki <43881003+mayur-22@users.noreply.github.com>
2019-01-13 22:52:59 +0530

    Add files via upload

---
+++ multiplicarion.h
@@ -0,0 +1,14 @@
+#ifndef MULTIPLICATION_H
+#define MULTIPLICATION_H
+#include "Matrix.h"
+
+class multiplicarion
+{
+public:
+	multiplicarion();
+	std::vector<float> multiplication(Matrix &A, int m);
+	~multiplicarion();
+	
+};
+
+#endif
---
+++ multiplication.cpp
@@ -0,0 +1,19 @@
+#include "multiplication.h"
+
+std::vector<float> multiplication(Matrix &A, int m){
+	std::vector<float> ans;
+	int n = A.get_sizeofrow();
+
+	for (int i = 0; i < n-m+1 ; ++i){
+		for (int j = 0; j < n-m+1; ++j){
+			for (int u = 0; u < m; ++u){
+				for (int v = 0; v < m; ++v){
+					ans.push_back(A.get_sizeofrow(i+u,j+v));
+				}
+			}
+		}
+	}
+
+	return ans;
+
+}
