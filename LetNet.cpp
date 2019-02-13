6a331587bddacc1c25c26521849e81b8837330a8
kamalashraf1 <46621032+kamalashraf1@users.noreply.github.com>
2019-02-13 22:41:04 +0530

    Update LetNet.cpp

--- LetNet.cpp
+++ LetNet.cpp
@@ -13,7 +13,16 @@ LetNet::~LetNet(){
     
 }
 
+/*
+ This function provides the output for the first convolutional layer.
+ It takes as input a single channel image as input, reads the weights and biases from the file
+ "conv1.txt" and then generates the 20 output channels by calling conv_mult_withoutpadding for performing
+ the convolution and subsequently add_bias for adding the biases.
+ 
+ The output is provided as pointer to an array of pointers to dynamically allocated Matrix objects where each object represents
+ one output channel.
 
+*/
 Matrix ** LetNet::Convolution1(Matrix &image){
 	Matrix **result = new Matrix*[20];
 	Matrix C("conv1.txt",1,520);
@@ -21,28 +30,42 @@ Matrix ** LetNet::Convolution1(Matrix &image){
 	convolution conv;
 	for(int i=0;i<20;i++){
 		Matrix R((ptr+(i*25)),5);
-		// R.print_matrix();
-		// image.print_matrix();
 		result[i] = conv.conv_mult_withoutpadding(image,R);
-		// result[i]->print_matrix();
 		int x = result[i]->get_sizeofrow();
 		int y = result[i]->get_sizeofcolumn();
 		result[i]->add_bias(C.get_Element(0,500+i));
-		// result[i]->print_matrix();
+		
 	}
 	return result;
 }
 
+/*
+ This function defines the first max_pooling layer.
+ It takes as input the array of pointers to the Matrix objects that are effectively the distinct units of the input channel.
+ It then modifies the objects by allocating to them the output matrix objects from application of max_pooling with kernel size 2
+ and stride parameter 2.
+
+*/
+
 void LetNet::Pool1(Matrix **inputchannels){
 	for(int i=0;i<20;i++){
-			// inputchannels[i]->print_matrix();
 			int n=inputchannels[i]->get_sizeofcolumn();
 			inputchannels[i] = max_pooling(*(inputchannels[i]),2,2);
-			// inputchannels[i]->print_matrix();
+			
 		}
 
 }
 
+/*
+ This function provides the output for the first convolutional layer.
+ It takes as input an array of pointers to Matrix objects, each one of which represent, one input channel,reads the weights and biases from the file
+ "conv2.txt" and then generates the 500 output channels by calling conv_mult_withoutpadding for performing
+ the convolution and subsequently add_bias for adding the biases.
+ 
+ The output is provided as pointer to an array of pointers to dynamically allocated Matrix objects where each object represents
+ one output channel.
+
+*/
 Matrix **LetNet::Convolution2(Matrix **image){
 	Matrix **result = new Matrix*[50];
 	Matrix C("conv2.txt",1,25050);
@@ -50,23 +73,27 @@ Matrix **LetNet::Convolution2(Matrix **image){
 	convolution conv;
 	for(int i=0;i<50;i++){
 		Matrix R(ptr+(i*500),5);
-		// image[0]->print_matrix();
 		result[i]=conv.conv_mult_withoutpadding(*(image[0]),R);
 		for(int j=1;j<20;j++){
 			Matrix kernel(ptr+(i*500)+(j*25),5);
 			Matrix *S=conv.conv_mult_withoutpadding(*(image[j]),kernel);
-			// result[i]->print_matrix();
 			(result[i])->add_matrix(*S);
 		}
-		// result[i]->print_matrix();
 		result[i]->add_bias(C.get_Element(0,25000+i));
 
 	}
-	int x =result[0]->get_sizeofrow();
 	return result;
 }
 
 
+
+/*
+ This function defines the second max_pooling layer.
+ It takes as input the array of pointers to the Matrix objects that are effectively the distinct units of the input channel.
+ It then modifies the objects by allocating to them the output matrix objects from application of max_pooling with kernel size 2
+ and stride parameter 2.
+
+*/
 void LetNet::Pool2(Matrix **inputchannels){
 	for(int i=0;i<50;i++){
 			inputchannels[i] = max_pooling(*(inputchannels[i]),2,2);
@@ -74,11 +101,23 @@ void LetNet::Pool2(Matrix **inputchannels){
 
 }
 
+/*
+ This function is simply used for the ReLU layer and just modifies each channel in the input by applying ReLU to each object.
+*/
+
 void LetNet::ReluAll(Matrix **inputchannels){
 	for(int i=0;i<500;i++){
 			inputchannels[i] = Relu_mat(*(inputchannels[i]));
 		}
 }
+
+/*
+ This function forms the first fully connected convolutional layer.
+ Though this could have been implemented much more simply we have maintained the same set of abstractions that were used for
+ the first tow convolutional layers.
+ 
+ There are 500 input channels and the same nummber of output channes associated with this layer.
+*/
 
 Matrix **LetNet::FullyConnected1(Matrix **image){
 	Matrix **result = new Matrix*[500];
@@ -99,6 +138,14 @@ Matrix **LetNet::FullyConnected1(Matrix **image){
 }
 
 
+/*
+ This function forms the second fully connected convolutional layer.
+ Though this could have been implemented much more simply we have maintained the same set of abstractions that were used for
+ the first tow convolutional layers.
+ 
+ There are 500 input channels and 10 output channes associated with this layer where each output channel corresponds to one digit.
+*/
+
 
 Matrix **LetNet::FullyConnected2(Matrix **image){
 	Matrix **result = new Matrix*[10];
@@ -110,14 +157,10 @@ Matrix **LetNet::FullyConnected2(Matrix **image){
 		result[i]=conv.conv_mult_withoutpadding(*(image[0]),R);
 		for(int j=1;j<500;j++){
 			Matrix kernel(ptr+(i*500)+j,1);
-			// kernel.print_matrix();
-			// image[j]->print_matrix();
 			Matrix *S=conv.conv_mult_withoutpadding(*(image[j]),kernel);
-			//S->print_matrix();
 			result[i]->add_matrix(*S);
 		}
 		result[i]->add_bias(C.get_Element(0,5000+i));
-		// result[i]->print_matrix();
 	}
 	return result;
 }
